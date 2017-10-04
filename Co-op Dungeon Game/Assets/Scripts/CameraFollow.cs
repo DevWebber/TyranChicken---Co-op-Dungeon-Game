8d0f5d75a248cdd7bb42743f1059f81c530c7fd1
Webber <DevWebber@users.noreply.github.com>
2017-10-04 11:51:53 +0100

    Basic Movement and Following Camera
    Adds the ability to move the player with the camera following in a 3rd person perspective

---
+++ Co-op Dungeon Game/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private float yOffset;
+    [SerializeField]
+    private float zOffset;
+	
+	void Update()
+    {
+        transform.position = new Vector3(target.position.x, yOffset, target.position.z - zOffset);
+
+	}
+}
---
+++ Co-op Dungeon Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    float xAxis;
+    float zAxis;
+    [SerializeField]
+    private float movementSpeed;
+	
+	void Update()
+    {
+        xAxis = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
+        zAxis = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
+
+        transform.Translate(xAxis, 0, zAxis);
+	}
+}
