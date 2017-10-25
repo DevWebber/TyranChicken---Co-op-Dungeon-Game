3c9c1e7fc9afbfa21b92aa01f8031ab08b07cceb
Webber <the.webster@hotmail.com>
2017-10-25 12:01:45 +0100

    Added NavMesh
    Added the navigation system that will be used for players and enemies

--- Co-op Dungeon Crawler/Assets/Scripts/PlayerMovement.cs
+++ Co-op Dungeon Crawler/Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerMovement : MonoBehaviour {
 
@@ -8,12 +9,25 @@ public class PlayerMovement : MonoBehaviour {
     float v_zAxis;
     [SerializeField]
     private float v_movementSpeed;
-	
+
+	NavMeshAgent agent;
+
+	void Start()
+	{
+		agent = GetComponent<NavMeshAgent> ();
+	}
+
 	void Update()
     {
+
+		
         v_xAxis = Input.GetAxis("Horizontal") * Time.deltaTime * v_movementSpeed;
         v_zAxis = Input.GetAxis("Vertical") * Time.deltaTime * v_movementSpeed;
 
+        agent.Move(new Vector3(v_xAxis, 0, v_zAxis));
+
+        /*
         transform.Translate(v_xAxis, 0, v_zAxis);
+        */
 	}
 }
